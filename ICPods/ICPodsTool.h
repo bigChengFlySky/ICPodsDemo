e0025ded246b2d45b14aebaf1a6e4a184c8a0ed5
chengchengcui <478029574@qq.com>
2017-10-16 14:00:46 +0800

    12321

---
+++ ICPods/ICPodsTool.h
@@ -0,0 +1,13 @@
+//
+//  ICPodsTool.h
+//  ICPods
+//
+//  Created by cuichengcheng on 2017/10/16.
+//  Copyright © 2017年 cuichengcheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ICPodsTool : NSObject
+
+@end
---
+++ ICPods/ICPodsTool.m
@@ -0,0 +1,13 @@
+//
+//  ICPodsTool.m
+//  ICPods
+//
+//  Created by cuichengcheng on 2017/10/16.
+//  Copyright © 2017年 cuichengcheng. All rights reserved.
+//
+
+#import "ICPodsTool.h"
+
+@implementation ICPodsTool
+
+@end
